099476d92e4198e02594f8b106d1d7cf73f02625
alexey lashnev <a.lashnev@platron.ru>
2017-03-19 22:15:41 +0300

    Протестировано на отсутствие ошибок

--- platron/sig_helper.py
+++ platron/sig_helper.py
@@ -1,21 +1,24 @@
-import _md5
+import hashlib
 import xml.etree.ElementTree
 
 class SigHelper(object):
+    '''
+    Class to help make and check sign
+    '''
     
     flat_xml_array = {}
     flat_array = {}
     
-    '''
-    Class to help make and check sign
-    '''
     def __init__(self, secret_key):
         self.secret_key = secret_key
         
-    def __make_flat_params_xml(self, xml, parent_name = None):
-        if parent_name == None:
-            root = xml.etree.ElementTree.fromstring(xml)
+    def __make_flat_params_xml(self, xml_str, parent_name = ''):
         
+        if parent_name == '':
+            root = xml.etree.ElementTree.fromstring(xml_str)
+        else:
+            root = xml_str
+            
         i = 0
         for child in root:
             if child.text == 'pg_sig':
@@ -24,26 +27,26 @@ class SigHelper(object):
             name = parent_name + child.tag + str(i)
             
             if child.getchildren() == []:
-                self.__make_flat_params_xml(xml, name)
+                self.__make_flat_params_xml(child, name)
                 continue
             
             self.flat_xml_array.update({name : child.text})
             
         return self.flat_xml_array
         
-    def __make_flat_params_array(self, params, parent_name = None):
+    def __make_flat_params_array(self, params, parent_name = ''):
         i = 0
         for key in params:
             if key == 'pg_sig':
                 continue
             
-            name = parent_name + key + str(i)
+            name = parent_name + str(key) + str(i)
             
-            if type(params.get(key)) == dict:
+            if type(params.get(str(key))) == dict:
                 self.__make_flat_params_array(params, name)
                 continue
             
-            self.flat_array.update({key, params.get(key)})
+            self.flat_array.update({key : params.get(str(key))})
             
         return self.flat_array            
     
@@ -54,14 +57,14 @@ class SigHelper(object):
         str_to_sig = script_name
         
         for key in list_to_sort:
-            str_to_sig += params.get(key)
+            str_to_sig += str(params.get(str(key)))
             
         str_to_sig += self.secret_key
         return str_to_sig
     
     def make(self, script_name, params):
         flat_params = self.__make_flat_params_array(params)
-        return _md5.md5(self.__make_sig_str(script_name, flat_params))
+        return hashlib.md5(self.__make_sig_str(script_name, flat_params).encode('utf-8')).hexdigest()
                         
     def check(self, signature, script_name, xml):
         return signature == self.make_xml(script_name, xml)
