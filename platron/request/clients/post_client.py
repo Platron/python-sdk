cc9f35b914d728b1680002b90937afa86c6fa0b1
alexey lashnev <a.lashnev@platron.ru>
2017-03-19 23:44:23 +0300

    Клиент для POST запросов

--- platron/request/clients/post_client.py
+++ platron/request/clients/post_client.py
@@ -1,4 +1,7 @@
+import requests
 from .client import Client
+from platron.exception import SdkException
+import xml.etree.ElementTree
 
 class PostClient(Client):
     '''
@@ -11,4 +14,19 @@ class PostClient(Client):
         self.secret_key = secret_key
     
     def request(self, request_builder):
-        return None
+        try :
+            response = requests.post(request_builder.get_url(), request_builder.get_params())      
+            root = xml.etree.ElementTree.fromstring(response)
+        except Exception:
+            raise SdkException('Cant send request')
+
+        for child in root:
+            if child.tag == 'pg_error':
+                pg_error = child.text
+            if child.tag == 'pg_error_code':
+                pg_error_code = child.text
+                
+        if pg_error_code:
+            raise SdkException('Error in response ' + pg_error + 'code ' + pg_error_code)
+        
+        return root
---
+++ setup.py
@@ -0,0 +1,18 @@
+import codecs
+from setuptools import setup, find_packages
+
+with codecs.open('README.rst', 'r', 'utf-8') as file:
+    long_description = file.read()
+    
+setup(
+    name = 'platron',
+    version = '1.0.0',
+    packages = find_packages(exclude=['tests']),
+    install_requires = ['requests>=2.6', 'six'],
+    author = 'Platron',
+    author_email = 'it@platron.ru',
+    description = 'A client library for the Platron API',
+    long_description = long_description,
+    keywords = 'platron payment api',
+    url = 'https://github.com/Platron/python-sdk',
+)
