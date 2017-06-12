8b23d7d9a04ee25700cfa4a2bd2e012493a036a5
alexey lashnev <a.lashnev@platron.ru>
2017-06-12 10:00:41 +0300

    No test file

--- test.py
+++
@@ -1,6 +0,0 @@
-from platron.request.request_builders.init_payment_builder import InitPaymentBuilder
-from platron.request.clients.post_client import PostClient
-
-client = PostClient('82', 'sdcacsdcasdc')
-init_payment_builder = InitPaymentBuilder('10.00', 'test')
-print(client.request(init_payment_builder))
