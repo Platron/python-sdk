7e225d68d7557b98c58984d72eadacb34978bd92
lashnev <a.lashnev@platron.ru>
2018-11-09 17:19:36 +0300

    New version in setup

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 import codecs
 from setuptools import setup, find_packages
-    
+
 setup(
-    name = 'platron',
-    version = '2.0.2',
-    packages = ['platron','platron.request.request_builders','platron.request.data_objects','platron.request.clients','platron.callback'],
-    install_requires = ['requests>=2.6', 'six', 'dicttoxml'],
-    author = 'Platron',
-    author_email = 'it@platron.ru',
-    description = 'A client library for the Platron API',
-    keywords = 'platron payment api',
-    url = 'https://github.com/Platron/python-sdk',
+    name='platron',
+    version='2.2.0',
+    packages=['platron', 'platron.request.request_builders', 'platron.request.data_objects', 'platron.request.clients',
+              'platron.callback'],
+    install_requires=['requests>=2.6', 'six', 'dicttoxml'],
+    author='Platron',
+    author_email='it@platron.ru',
+    description='A client library for the Platron API',
+    keywords='platron payment api',
+    url='https://github.com/Platron/python-sdk',
 )
