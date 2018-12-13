0173e840d7c4508ab0ba50174e0b01c70df96bff
lashnev <a.lashnev@platron.ru>
2018-12-13 17:19:37 +0300

    Add partial clearing param

--- platron/request/request_builders/do_capture_builder.py
+++ platron/request/request_builders/do_capture_builder.py
@@ -22,5 +22,11 @@ class DoCaptureBuilder(RequestBuilder):
         for param_name in long_record_params.keys():
             setattr(self, param_name, long_record_params.get(param_name))
 
+    def add_amount(self, amount):
+        """
+        :param amount: clearing amount (string)
+        """
+        self.pg_amount = amount
+
     def get_url(self):
         return self.PLATRON_URL + 'do_capture.php'
